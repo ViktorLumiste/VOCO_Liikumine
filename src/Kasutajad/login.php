93f3cd5cd8ed888653935e01727e72c2ef7e48b1
ViktorLumiste <viktor.lumiste@gmail.com>
2024-02-07 16:46:59 +0200

    Updated the login to use sessionId's

--- src/Kasutajad/login.php
+++ src/Kasutajad/login.php
@@ -6,25 +6,31 @@ $database = "lumisteviktor_VOCO_Liikumine";
 $username = "lumisteviktor";
 $password = "2$9?,bzk+VN0";
 
-
 // Create connection for reading data
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 $username = $_POST['loginName'];
 $password = $_POST['loginPassword'];
+
 // Perform a simple query to check user credentials
 $sql = "SELECT Parool FROM KASUTAJAD WHERE Nimi='$username'";
 $result = $conn->query($sql);
 $results = mysqli_fetch_assoc($result);
 $resultstring = $results['Parool'];
-$unhashed = password_verify($password,$resultstring);
-
+$unhashed = password_verify($password, $resultstring);
 
 // Check if the query was successful
 if ($unhashed) {
+    // Start PHP session
+    session_start();
+
+    // Set session variables
+    $_SESSION['username'] = $username;
+
     // User authentication successful
     http_response_code(200); // OK
     echo "Login successful!";
@@ -33,6 +39,7 @@ if ($unhashed) {
     http_response_code(401); // Unauthorized
     echo "Login failed. Invalid username or password.";
 }
+
 // Close the database connection
 $conn->close();
 ?>
