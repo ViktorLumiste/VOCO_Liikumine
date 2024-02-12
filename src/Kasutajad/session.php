da4651c310af376e7a99c834a93bb6650a37f75c
ViktorLumiste <viktor.lumiste@gmail.com>
2024-02-12 22:04:57 +0200

    Updated session.php to be more versatile

--- src/Kasutajad/session.php
+++ src/Kasutajad/session.php
@@ -1,13 +1,14 @@
 <?php
+$c_name = $_POST['cname'];
 // Start PHP session
 session_start();
 
 // Check if the user is logged in
-if (isset($_SESSION['username'])) {
+if (isset($_SESSION[$c_name])) {
     http_response_code(200);
     // Return a username from the session
-    $username = $_SESSION['username'];
-    echo $username;
+    $return = $_SESSION[$c_name];
+    echo $return;
 } else {
     // Redirect the user to the login page if not logged in
     http_response_code(401);
