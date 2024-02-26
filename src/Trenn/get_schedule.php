a84e8bce72d10270041ea29cdf3555459273a438
ViktorLumiste <viktor.lumiste@gmail.com>
2024-02-26 15:26:17 +0200

    Made the final version of schedule functionality

--- src/Trenn/get_schedule.php
+++ src/Trenn/get_schedule.php
@@ -1,12 +1,19 @@
 <?php
+// Retrieve data from the POST request
+$data = json_decode(file_get_contents('php://input'), true);
+
 // Database credentials
 require '../Database.php';
 global $conn;
 
 // Fetch schedule data from the database
-$query = "SELECT TRENNI_INFO.Tegevus as activityName, DATE_FORMAT(TRENNID.Algab, '%H:%i') AS start_time, DATE_FORMAT(TRENNID.Lopeb, '%H:%i') AS end_time, DAYNAME(TRENNID.Paev) AS weekday FROM TRENNID JOIN TRENNI_INFO ON TRENNID.Trenni_ID = TRENNI_INFO.Trenni_ID ORDER BY start_time";
+$query = "SELECT TRENNI_INFO.Tegevus as activityName, DATE_FORMAT(TRENNID.Algab, '%H:%i') AS start_time, DATE_FORMAT(TRENNID.Lopeb, '%H:%i') AS end_time, DAYNAME(TRENNID.Paev) AS weekday, TRENNID.Asukoht as placeName, TRENNI_INFO.Kellele as targetName FROM TRENNID JOIN TRENNI_INFO ON TRENNID.Trenni_ID = TRENNI_INFO.Trenni_ID where (week(TRENNID.Paev)+1)=$data[weekNum] ORDER BY start_time";
 $result = $conn->query($query);
 
+// Check if the query was successful
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
 
 // Initialize arrays for each weekday
 $weekdayArrays = array(
@@ -22,10 +29,11 @@ while ($row = $result->fetch_assoc()) {
     $weekday = $row['weekday'];
     $weekdayArrays[$weekday][] = $row;
 }
+
 // Close the connection
 $conn->close();
 
 // Send JSON response
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($weekdayArrays);
 ?>
