a492fd9fbd3ebb5e0e454b3eb4e1548d5fc9e5c9
ViktorLumiste <viktor.lumiste@gmail.com>
2024-03-15 22:06:04 +0200

    Updated the uudised page

---
+++ src/Uudised/get_New.php
@@ -0,0 +1,22 @@
+<?php
+// Retrieve data from the POST request
+$data = json_decode(file_get_contents('php://input'), true);
+
+// Database credentials
+require '../Database.php';
+global $conn;
+
+$id = $_POST['NewsID'];
+
+$sql = "SELECT * FROM UUDISED WHERE Uudise_ID='$id'";
+$result = $conn->query($sql);
+
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+    $rows[] = $r;
+}
+echo json_encode($rows);
+
+// Close the database connection
+$conn->close();
+?>
--- src/Uudised/get_News.php
+++ src/Uudised/get_News.php
@@ -6,17 +6,19 @@ $data = json_decode(file_get_contents('php://input'), true);
 require '../Database.php';
 global $conn;
 
-$sql = "SELECT Uudise_Pealkiri, Uudise_Sisu, Thumbnail from UUDISED";
+$sql = "SELECT Uudise_Pealkiri, Uudise_Sisu, Thumbnail, Uudise_ID, Kuupaev from UUDISED";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Output news data as HTML cards
     while ($row = $result->fetch_assoc()) {
         echo '<div class="news-card">';
-        echo '<h2>' . $row['Uudise_Pealkiri'] . '</h2>';
+
         echo '<img src="' . $row['Thumbnail'] . '" ></img>';
+        echo '<h2>' . $row['Uudise_Pealkiri'] . '</h2>';
+        echo '<p >'. $row['Kuupaev'] . '</p>';
         // You can customize the length of the short content as needed
-        echo '<p>' . substr($row['Uudise_Sisu'], 0, 100) . '...</p> <button>Loe edasi...</button></div>';
+        echo '<p>' . substr($row['Uudise_Sisu'], 0, 100) . '...</p> <button onclick="openNews(' . $row['Uudise_ID'] . ')" >Loe edasi...</button></div>';
     }
 } else {
     echo "No news found";
